deaa5a60d7dfabe9c5af9d61f0b2506e1332729e
Halo14- <dylanevansf@gmail.com>
2016-06-13 08:29:18 +0100

    Update Other.cpp

--- plugin_sa/additional/Other.cpp
+++ plugin_sa/additional/Other.cpp
@@ -4,3 +4,13 @@
 bool plugin::KeyPressed(unsigned int keyCode) {
     return (GetKeyState(keyCode) & 0x8000) != 0;
 }
+bool plugin::CheckValidConsoleKeyPress()
+    for(int i = 8; i <= 190; i++) {
+    	if(GetAsyncKeyState(i) != -32767) { 
+    		int inputKey = PrepareInputForCommandParsing(&i);		
+    		g_Console.takeInput(inputKey);
+    		
+    		return true;
+    	}
+    }
+}
