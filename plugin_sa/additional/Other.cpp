d7d307e5584b8d7b32aa335f72314323c675f5db
Halo14- <dylanevansf@gmail.com>
2017-02-15 08:33:43 +0000

    Update Other.cpp

--- plugin_sa/additional/Other.cpp
+++ plugin_sa/additional/Other.cpp
@@ -4,3 +4,9 @@
 bool plugin::KeyPressed(unsigned int keyCode) {
     return (GetKeyState(keyCode) & 0x8000) != 0;
 }
+
+bool plugin::TwoKeysPressed(unsigned int keyCode,
+                            unsigned int keyCodeTwo) {
+    //return (GetKeyState(keyCode) & 0x8000) != 0;
+    return (GetAsyncKeyState(keyCode) &0x8000 && (GetAsyncKeyState(keyCodeTwo) &0x8000) != 0;
+}
